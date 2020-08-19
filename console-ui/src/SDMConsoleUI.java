fc9d143751465f57092072bf0f1c5b440010f12d
danielsapir <danielsap123@gmail.com>
2020-08-19 18:53:54 +0300

    Daniel main menu Option fullfiled

--- console-ui/src/SDMConsoleUI.java
+++ console-ui/src/SDMConsoleUI.java
@@ -1,5 +1,20 @@
 public class SDMConsoleUI
 {
-    SDMEngine engine;
+    private SDMEngine engine;
+    private final String[] mainMenu = {"Load new XML file.","Show all stores information","Show all items information.","Make new order.","Show all orders.","Exit."};
+
+    public void start() {
+        showMenu();
+
+    }
+
+    private void showMenu() {
+        for (int i = 0; i < mainMenu.length; i++) {
+            System.out.println(i+1 + mainMenu[i]);
+        }
+
+
+    }
+
 
 }
